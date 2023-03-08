b5b7a1814233c981aec5db247957e98c114bbbf9
bjurinformatik <josefin.bjurling@igp.uu.se>
2023-03-08 13:52:53 +0100

    Updated script

--- day3/classroom.py
+++ day3/classroom.py
@@ -15,11 +15,20 @@ class Person():
     def __str__(self):
         return "The name of the person is %s %s." % (self.firstname, self.lastname)
 
+#Add class called Student that inherit from the Person class
 class Student(Person):
     def __init__(self, firstname, lastname, subject_area):
       Person.__init__(self, firstname, lastname)
       self.subject_area = subject_area
-    
-# Add function that returns the 
+     
     def printNameSubject(self):
         return(self.firstname, self.lastname, self.subject_area)
+
+#Add another class Techer that also inherit from Person class
+class Teacher(Person):
+    def __init__(self, firstname, lastname, course):
+      Person.__init__(self, firstname, lastname)
+      self.course = course
+
+    def printTeacherCourse(self):
+        return(self.firstname, self.lastname, self.course)
