db1fd65ab2a7eb7b99daf4169545989e72f8e4ed
bjurinformatik <josefin.bjurling@igp.uu.se>
2023-03-08 13:11:27 +0100

    Create folder for day 2 tasks

---
+++ day2/animals/__init__.py
@@ -0,0 +1,6 @@
+#! /usr/bin/env python
+
+#Import modules to package
+from .mammals import Mammals
+from .harmless import birds
+from .dangerous import fish
---
+++ day2/test_animals.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+
+import animals
+
+m = animals.Mammals()
+m.printMembers()
+
+b = animals.Birds()
+b.printMembers()
